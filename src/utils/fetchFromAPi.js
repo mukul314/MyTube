e398df925179870f06ae0826b52944cf5941dded
mukul314 <mukulsharma3779@gmail.com>
2023-06-14 21:23:40 +0530

    changed the key

--- src/utils/fetchFromAPi.js
+++ src/utils/fetchFromAPi.js
@@ -1,14 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+;
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': '30fe5f8512msh69646cada7639fdp129c3ajsn2d2b52956874',
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': '91454f189amsh53422da7e86ca25p113428jsn0307e0fb9a68',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   },
 };
 
@@ -18,4 +19,4 @@ export const fetchFromAPI = async (url) => {
   return data;
 };
 
-  //'30fe5f8512msh69646cada7639fdp129c3ajsn2d2b52956874'
+//'30fe5f8512msh69646cada7639fdp129c3ajsn2d2b52956874'
